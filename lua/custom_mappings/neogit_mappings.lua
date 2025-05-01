fea54d729387dfbbd2f8c557eb9bc2080ecd4bbf
tienpvse1 <good66612@gmail.com>
2025-05-01 23:09:11 +0700

    enhance: add git mappings

--- lua/custom_mappings/neogit_mappings.lua
+++ lua/custom_mappings/neogit_mappings.lua
@@ -4,6 +4,10 @@ map("n", "<leader>gg", function()
   require("neogit").open { kind = "floating" }
 end, { desc = "Git open" })
 
+map("n", "<leader>gs", function()
+  require("neogit").open { "stash", kind = "floating" }
+end, { desc = "Git open" })
+
 map("n", "<leader>Gd", function()
   if next(require("diffview.lib").views) == nil then
     vim.cmd("DiffviewOpen", {})
@@ -39,3 +43,15 @@ end, { desc = "Git Toggle blame line" })
 map("n", "<leader>gB", function()
   require("gitsigns").blame()
 end, { desc = "Git Open Blame" })
+
+map("n", "<leader>gh", function()
+  require("gitsigns").preview_hunk_inline()
+end, { desc = "Git Hunk inline" })
+
+map("n", "<leader>gfh", function()
+  require("gitsigns").preview_hunk()
+end, { desc = "Git Hunk floating" })
+
+map("n", "<leader>gq", function()
+  require("gitsigns").setqflist()
+end, { desc = "Git Hunk floating" })
